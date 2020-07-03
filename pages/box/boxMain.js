7fede00e9eb12db1832efe58f1d5789a456e5bc7
pengchihui1 <pengchihui1@aliyun.com>
2020-07-03 13:56:00 +0800

    屏幕 手机  pc端

--- pages/box/boxMain.js
+++ pages/box/boxMain.js
@@ -68,8 +68,8 @@ const Leftk = function () {
         <Box width='30%'>
           <Text>毕业院校：</Text>
         </Box>
-        <Box width='50%'>
-          <Text>湖南科技大学</Text>
+        <Box width='60%'>
+          <Text>长沙华信智原电脑学院</Text>
         </Box>
       </Box>
       {/* 学历 */}
@@ -119,37 +119,40 @@ const Right = function () {
   )
 }
 
+const breakpoints = ['600px', '600px', '800px', '1000px']
+// aliases  1em=16px 字体 宽高 字体
+breakpoints.sm = breakpoints[0]
+breakpoints.md = breakpoints[1]
+breakpoints.lg = breakpoints[2]
+breakpoints.xl = breakpoints[3]
+
 const App = () => (
   <ThemeProvider>
     <CSSReset />
     <Box>
-      <Box width='1000px' border='1px solid #7d7d7d' height='600px' margin='0 auto' mt='20px' padding='20px'>
-
+      <Box width={['100%', '600px', '800px', '1000px']} border={{ sm: 'none', md: '1px solid #7d7d7d' }} h margin='0 auto' mt='20px' padding='20px'>
         <Box padding='20px' backgroundColor='yellow ' opacity='0.4' color='black'>
           <Text display='flex' justifyContent='center' fontSize='24px' b>
             个人简历
           </Text>
         </Box>
 
-        <Box height='auto' display='flex' justifyContent='space-between ' mt='20px'>
+        <Box height='auto' display={{ sm: 'block', md: 'flex' }} justifyContent='space-between ' mt='20px'>
           <Box
-            width='30%'
-            height='400px'
+            width={{ sm: '100%', md: '30%' }}
+            h
             display='flex'
             flexDirection='column'
             justifyContent='flex-start'
-
             padding='0 5px 5px 5px'
           >
             <Leftk />
           </Box>
 
-          <Box width='65%'>
+          <Box width={{ sm: '100%', md: '60%' }}>
             <Right />
           </Box>
         </Box>
-        {/* 职业技能 */}
-        <Box marginTop='10px' height='50px' />
 
       </Box>
     </Box>
